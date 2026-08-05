118681553de24e0a0f1c30a189e0597df100e011
Elizabeth Pezzuto <eapezzuto@gmail.com>
2026-08-05 14:35:27 -0400

    Made a menu loop

---
+++ dev/Project1/Cooking.cpp
@@ -0,0 +1,1 @@
+#include "Cooking.h"
--- dev/Project1/Project1.cpp
+++ dev/Project1/Project1.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Welcome to Secret Ingrediant!\n";
+    bool running = true;
+
+    std::string input;
+    int choice;
+
+    while (running)
+    {
+        std::cout << "What would you like to do? \n1) PLAY \n2) EXIT \n\n";
+        std::cin >> input;
+        choice = std::stoi(input);
+
+        switch (choice)
+        {
+        case 1:
+            std::cout << "The game is playing\n";
+            break;
+        case 2:
+            std::cout << "Goodbye\n";
+            running = false;
+            break;
+        default:
+            std::cout << "Whoopsie, you entered something in INCORRECTLY. >:C \n";
+            break;
+        }
+    }
 }
