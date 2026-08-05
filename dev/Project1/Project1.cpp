828b7c876a3163036379eb0c0026007864d1d0d6
Elizabeth Pezzuto <eapezzuto@gmail.com>
2026-08-05 14:42:35 -0400

    The clear screen is working

--- dev/Project1/Project1.cpp
+++ dev/Project1/Project1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 
+void clear(); //forward declration to clear screen
+
 int main()
 {
-    std::cout << "Welcome to Secret Ingrediant!\n";
-    bool running = true;
+    std::cout << "Welcome to Secret Ingrediant!\n"; //title of game
+    bool running = true; //makes game run
 
-    std::string input;
+    std::string input; //will hold user input and change to menu choice
     int choice;
 
     while (running)
@@ -15,18 +17,24 @@ int main()
         std::cin >> input;
         choice = std::stoi(input);
 
-        switch (choice)
+        switch (choice) //actual menu directory
         {
         case 1:
-            std::cout << "The game is playing\n";
+            std::cout << "The game is playing\n\n";
             break;
         case 2:
             std::cout << "Goodbye\n";
             running = false;
             break;
-        default:
-            std::cout << "Whoopsie, you entered something in INCORRECTLY. >:C \n";
+        default: //entered something invalid
+            clear();
+            std::cout << "Whoopsie, you entered something in INCORRECTLY. >:C \n\n";
             break;
         }
     }
+}
+
+void clear()
+{
+    std::cout << "\033[H\033[2J" << std::flush;
 }
