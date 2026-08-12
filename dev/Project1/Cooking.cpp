b124e4724302ff16a6cc774366f587e08dd626be
Elizabeth Pezzuto <eapezzuto@gmail.com>
2026-08-12 14:30:18 -0400

    Reworked the cooking menu so I can add options more easily.

--- dev/Project1/Cooking.cpp
+++ dev/Project1/Cooking.cpp
@@ -11,7 +11,8 @@ void Cooking::startCooking()
 
     std::string input; //will hold user input and change to menu choice
     int choice;
-    Cooking myFood;
+    Cooking myFood; //what we are adding to
+    Cooking options; //the list of ingredients to choose from
 
     //the ingrediant options
     Ingrediant egg("Egg");
@@ -19,44 +20,39 @@ void Cooking::startCooking()
     Ingrediant flour("Flour");
     Ingrediant sugar("Sugar");
 
+    options.addToBowl(egg);
+    options.addToBowl(milk);
+    options.addToBowl(flour);
+    options.addToBowl(sugar);
+
     for (int count = 0; count < 4; count++)
     {
+
         std::cout << "You may add only 4 Ingredients\n"; //ingredient cap meets for loop conditions
-        std::cout << "would you like to add to the bowl? \n1) Eggs \n2) Milk \n3) Flour \n4) Sugar \n\n";
+        std::cout << "would you like to add to the bowl? \n";
+        int num = 1;
+        for (auto& o : options.getIngrediants()) //shows list of options
+        {
+            std::cout << num << ") " << o.getName() << "\n"; 
+            num += 1;
+        }
         std::cin >> input;
         choice = std::stoi(input);
 
-        switch (choice) //menu directory for adding
+        if (choice < 0 || choice > options.bowl_.size() + 1) //checks if this answer is invalid
         {
-        case 1:
-            Graphics::clear();
-            std::cout << "Egg added\n";
-            myFood.addToBowl(egg);
-            break;
-        case 2:
-            Graphics::clear();
-            std::cout << "Milk added\n";
-            myFood.addToBowl(milk);
-            break;
-        case 3:
-            Graphics::clear();
-            std::cout << "Flour added\n";
-            myFood.addToBowl(flour);
-            break;
-   
-        case 4:
-            Graphics::clear();
-            std::cout << "Sugar added\n";
-            myFood.addToBowl(sugar);
-            break;
-        default: //entered something invalid
             Graphics::clear();
             std::cout << "You added air, it did nothing. \n\n";
             count--; //this was not actually an ingredient....
-            break;
+        }
+        else //converts user answer to ingredient
+        {
+            Graphics::clear();
+            std::cout << options.bowl_.at(choice -1).getName() <<" added\n";
+            myFood.addToBowl(options.bowl_.at(choice - 1));
         }
     }
-    //outside of for loop so this happens only after for loop conditions are met.
+
     std::cout << "mixing ingrediants together...\n\n"; 
     for (auto& b : myFood.getIngrediants()) //see what we are mixing together
     {
